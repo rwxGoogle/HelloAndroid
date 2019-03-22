c009499202a41a74b88f90a2462b597c1981c35a
Roxana Nistor <roxana_nistor_gemini_solutions_inc@whirlpool.com>
2019-03-22 10:09:51 +0200

    Created first challenge from Week2

--- app/src/main/java/com/example/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/helloandroid/MainActivity.java
@@ -1,53 +1,179 @@
 package com.example.helloandroid;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RatingBar;
+import android.widget.SeekBar;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String ANDROID_LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
+        // Examples are below. Please uncomment ONE LINE AT A TIME !
 
-        fab.setOnClickListener(new View.OnClickListener() {
+       // textViewExample();
+     //   imageViewExample();
+       // buttonExample();
+      //  viewExample();
+      //  editTextExample();
+       // checkBoxExample();
+       // radioButtonExample();
+       // ratingBarExample();
+      //  switchExample();
+      //  seekbarExample();
+       // progressBarExample();
+    }
+
+    private void textViewExample() {
+        setContentView(R.layout.activity_main_textview);
+        // get the text from the label
+        TextView label = findViewById(R.id.label);
+        String labelText = label.getText().toString();
+
+
+       // label.setText("Hello Android Developers");
+
+        Log.d(ANDROID_LOG_TAG, labelText);
+    }
+
+    private void imageViewExample() {
+        setContentView(R.layout.activity_main_imageview);
+    }
+
+    private void buttonExample() {
+        setContentView(R.layout.activity_main_button);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Button pressed!", Toast.LENGTH_LONG).show();
             }
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    private void viewExample() {
+        setContentView(R.layout.activity_main_view);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    private void editTextExample() {
+        setContentView(R.layout.activity_main_edit_text);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        ((EditText) findViewById(R.id.inputbox)).addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
 
-        return super.onOptionsItemSelected(item);
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                Log.d(ANDROID_LOG_TAG, "Text in edit text is: " + s.toString());
+                Toast.makeText(MainActivity.this, s.toString(), Toast.LENGTH_LONG).show();
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+    }
+
+    private void checkBoxExample() {
+        setContentView(R.layout.activity_main_checkbox);
+
+        ((CheckBox) findViewById(R.id.checkbox)).setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Log.d(ANDROID_LOG_TAG, "Checkbox has changed : " + (isChecked ? "Checked" : "Unchecked"));
+            }
+        });
+
+    }
+
+    private void radioButtonExample() {
+        setContentView(R.layout.activity_main_radio_buttons);
+
+        ((RadioGroup) findViewById(R.id.radio_group)).setOnCheckedChangeListener(
+                new RadioGroup.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(RadioGroup group, int checkedId) {
+                        RadioButton checkedButton = group.findViewById(checkedId);
+                        Log.d(ANDROID_LOG_TAG, "The checked radio button is : " + checkedButton.getText().toString());
+                    }
+                });
+    }
+
+    private void ratingBarExample() {
+        setContentView(R.layout.activity_main_rating_bar);
+
+        ((RatingBar) findViewById(R.id.rating_bar)).setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                Log.d(ANDROID_LOG_TAG, "Rating has changed to: " + rating + " stars");
+            }
+        });
+    }
+
+    private void switchExample() {
+        setContentView(R.layout.activity_main_switch);
+
+        ((Switch) findViewById(R.id.switch_view)).setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Toast.makeText(MainActivity.this, "The switch is now: " + (isChecked ? "ON" : "OFF"), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void seekbarExample() {
+        setContentView(R.layout.activity_main_seekbar);
+
+        ((SeekBar) findViewById(R.id.seek_bar)).setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                Log.d(ANDROID_LOG_TAG, "Seek has been changed by user ? " + fromUser + "\nTo progress: " + progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                Log.d(ANDROID_LOG_TAG, "Seekbar tracking has started !");
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Log.d(ANDROID_LOG_TAG, "Seekbar has been touched !");
+            }
+        });
+    }
+
+    private void progressBarExample() {
+        setContentView(R.layout.activity_main_progressbar);
+
+        final ProgressBar progressBar = findViewById(R.id.progress_bar);
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (progressBar.getVisibility() == View.VISIBLE) {
+                    progressBar.setVisibility(View.INVISIBLE);
+                } else if (progressBar.getVisibility() == View.INVISIBLE) {
+                    progressBar.setVisibility(View.VISIBLE);
+                }
+            }
+        });
     }
 }
